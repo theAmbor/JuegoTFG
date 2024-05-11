902e518b3cdeaa8925cd4fdaf51ac34748b4019e
theAmbor <157710338+theAmbor@users.noreply.github.com>
2024-05-11 17:11:06 +0200

    Movimiento y rotación del jugador
    También añadí las animaciones de inactividad y de caminar.

--- Player.cs
+++ Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 7f; //Velocidad de movimiento
 
+    private bool isWalking;
 
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
@@ -33,7 +34,15 @@ public class Player : MonoBehaviour {
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y); //Creamos un vector3 y sustituimos los ejes x y z por las cordenadas del vector2
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-        Debug.Log(Time.deltaTime);
+        isWalking = moveDir != Vector3.zero; //Si la velocidad de movimiento es diferente de 0, la variable "estáCaminando" cambiará a true
+
+        float rotateSpeed = 10f; //La velocidad de rotacion del personaje
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed); //Actualizamos el "forward" para que el personaje gire en la dirección a la que está caminando
+       
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 
 }
