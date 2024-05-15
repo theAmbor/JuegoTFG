dee0b9d47bf2d066ee1488529d1d96d70e2d5cc0
theAmbor <157710338+theAmbor@users.noreply.github.com>
2024-05-15 18:09:42 +0200

    Hacer aparecer objetos y cogerlos
    Estos últimos días he estado programando las mecánicas básicas para hacer aparecer los objetos de cocina y que el jugador pueda cogerlos. Los objetos saben dónde están, es decir, quién es su objeto padre, si los está sujetando el jugador o encima de qué encimera están. Esto será muy útil en el futuro, cuando tengamos muchos tipos de encimeras e ingredientes diferentes. Si el jugador o una encimera ya tiene un objeto "encima", el juego no te permitirá poner o coger otro.

--- Player.cs
+++ Player.cs
@@ -1,36 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour, IKitchenObjectParent {
 
+
+    public static Player Instance { get; private set; }
+
+    //Evento que se dispara cuando se selecciona una estantería
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs {
+
+        public ClearCounter selectedCounter;
+
+    }
 
     [SerializeField] private float moveSpeed = 7f; //Velocidad de movimiento
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask countersLayerMask;
+    [SerializeField] private Transform kitchenObjectHoldPoint;
+
 
     private bool isWalking;
     private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
+    private KitchenObject kitchenObject;
+
+
+    private void Awake() {
+
+        if (Instance != null) {
+
+            Debug.LogError("Hay más de una instancia de Jugador");
+
+        }
+
+        Instance = this;
+
+    }
 
     private void Start() {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
     }
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        if (selectedCounter != null) {
 
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if (moveDir != Vector3.zero) {
-            lastInteractDir = moveDir;
-        }
-
-        float interactDistance = 2f;
-        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
-                // Has ClearCounter
-                clearCounter.Interact();
-            }
+            selectedCounter.Interact(this);
         }
     }
 
@@ -55,11 +72,21 @@ public class Player : MonoBehaviour {
         float interactDistance = 2f;
         if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+
                 //Tiene el componente ClearCounter
-                
+                if (clearCounter != selectedCounter) {
+                    SetSelectedCounter(clearCounter);
+                }
+
+            } else {
+                SetSelectedCounter(null);
             }
+
+        } else {
+            SetSelectedCounter(null);
         }
     }
+         
 
     private void HandleMovement() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
@@ -108,4 +135,35 @@ public class Player : MonoBehaviour {
 
     }
 
+    private void SetSelectedCounter(ClearCounter selectedCounter) {
+
+        this.selectedCounter = selectedCounter;
+
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
+
+            selectedCounter = selectedCounter
+
+        });
+
+    }
+
+    public Transform GetKitchenObjectFollowTransform() {
+        return kitchenObjectHoldPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject) {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject() {
+        return kitchenObject != null;
+    }
 }
